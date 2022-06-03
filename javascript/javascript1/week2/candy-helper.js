a8abfef845f7a89c56e4bc3c55cc751c592b4361
lokesh-sw <lokesh_swami@yahoo.co.uk>
2022-06-03 04:14:18 +0200

    Edited candy helper

--- javascript/javascript1/week2/candy-helper.js
+++ javascript/javascript1/week2/candy-helper.js
@@ -1,27 +1,38 @@
 const boughtCandyPrices = [];
-let candyPrice = 0;
 
 function addCandy(candyType, weight) {
-  if ((candyType = "sweet")) {
+  let candyPrice = 0;
+  if (candyType === "sweet") {
     candyPrice = weight * 0.5;
-    return boughtCandyPrices.push(candyPrice);
-  } else if ((candyType = "chocolate")) {
+  } else if (candyType === "chocolate") {
     candyPrice = weight * 0.7;
-    return boughtCandyPrices.push(candyPrice);
-  } else if ((candyType = "toffee")) {
+  } else if (candyType === "toffee") {
     candyPrice = weight * 1.1;
-    return boughtCandyPrices.push(candyPrice);
-  } else if ((candyType = "chewing-gum")) {
+  } else if (candyType === "chewing-gum") {
     candyPrice = weight * 0.03;
-    return boughtCandyPrices.push(candyPrice);
   }
+  boughtCandyPrices.push(candyPrice);
 }
+
+// now add some candy prices to array
 
 addCandy("sweet", 20);
 addCandy("chocolate", 50);
 addCandy("toffee", 15);
 addCandy("chewing-gum", 30);
-console.log(boughtCandyPrices);
+
+// makes sum of all prices added to array
+
+let sumArray = 0;
+for (let index = 0; index < boughtCandyPrices.length; index++) {
+  sumArray += boughtCandyPrices[index];
+}
+
+// see if the function adds up the prices by logging it
+
+console.log(sumArray);
+
+// works fine so move on to next part of assignment.
 
 const amountToSpend = Math.random() * 100;
 
@@ -33,7 +44,7 @@ function canBuyMoreCandy(total) {
   }
   if (total < amountToSpend) {
     console.log("You can buy more, so please do!");
-  } else if (total > amountToSpend) {
+  } else {
     console.log("Enough candy for you!");
   }
 }
