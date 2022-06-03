18c50ed7a26357980609dec6823cd4fa8c395dc1
lokesh-sw <lokesh_swami@yahoo.co.uk>
2022-06-03 03:36:34 +0200

    Final event application

--- javascript/javascript1/week2/event-application.js
+++ javascript/javascript1/week2/event-application.js
@@ -11,10 +11,10 @@ const weekDay = [
 function getEventWeekday(numberDay) {
   const day = new Date();
   let today = day.getDay();
-  let eventDay = [(today + numberDay - 1) % 7];
+  let eventDay = (today + numberDay - 1) % 7;
   return weekDay[eventDay];
 }
 
-console.log(getEventWeekday(3)); //should log out Thursday
+console.log(getEventWeekday(3));
 
-console.log(getEventWeekday(4)); // should log out friday
+console.log(getEventWeekday(4));
