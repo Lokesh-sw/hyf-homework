0f52bf0a6a7727792b3bd0f3d6acc5323a0a8537
lokesh-sw <lokesh_swami@yahoo.co.uk>
2022-08-29 20:58:57 +0200

    additional query modified

--- databases/week2/homework_additionalqueries.sql
+++ databases/week2/homework_additionalqueries.sql
@@ -1,46 +1,52 @@
 -- Get all the tasks assigned to users whose email ends in @spotify.com
 
-SELECT
-    task.title, user.email
-FROM
-    task
-JOIN
-    user
-WHERE
-    user.email LIKE "%@spotify.com";
+SELECT 
+  task.* 
+FROM 
+  task 
+  JOIN user_task ON task.id = user_task.task_id 
+  JOIN user ON user_task.user_id = user.id 
+WHERE 
+  user.email LIKE "%@spotify.com";
     
  
  -- Get all the tasks for 'Donald Duck' with status 'Not started'
  
- SELECT 
-	task.title, task.status_id
- FROM
-	task
- JOIN 
-	user
-WHERE 
-	user.name LIKE "%Donald Duck%" AND task.status_id = 1;
+SELECT
+  task.*
+FROM
+  task
+  JOIN user_task ON task.id = user_task.task_id 
+  JOIN user ON user_task.user_id = user.id 
+  JOIN status ON status.id = task.status_id 
+where
+  user.name = 'Donald Duck'
+  AND status.name = 'Not started';
     
 
 -- Get all the tasks for 'Maryrose Meadows' that were created in september (hint: month(created)=month_number)
 
 SELECT
+    user.name,
     task.title,
-    task.created,
-    user.name
+    task.created
 FROM
     task
 JOIN
-    user
+    user_task ON 
+    task.id = user_task.task_id
+JOIN
+user ON user.id = user_task.user_id
+
 WHERE
-    user.name = "Maryrose Meadows" AND month(created) = 9;
+    user.name LIKE 'Maryrose Meadows' AND MONTH(created) = 09;
     
  
  -- Find how many tasks where created in each month, e.g. how many tasks were created in october, 
  -- how many tasks were created in november, etc. (hint: use group by)
  
 SELECT
-  month(task.created) as month, COUNT(task.id) as task_total
+  monthname(task.created) as month, COUNT(task.id) as task_total
 FROM
   task
 GROUP BY month;
