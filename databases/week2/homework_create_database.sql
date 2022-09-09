baa37607bad64b9a1a908e6c5d0bb3eef581a59a
lokesh-sw <lokesh_swami@yahoo.co.uk>
2022-09-09 17:45:02 +0200

    updated databse

--- databases/week2/homework_create_database.sql
+++ databases/week2/homework_create_database.sql
@@ -4,26 +4,30 @@ USE `hyf-library`;
 
 -- Create data table
 CREATE TABLE `students` (
-    `student_id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     `first_name` varchar(100) NOT NULL,
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     `first_name` varchar(100) NOT NULL ,
      `lastname` varchar(100) NOT NULL,
-     `email` varchar(100) NOT NULL,
+     `email` varchar(100) NOT NULL UNIQUE,
      `phone` int(10) NOT NULL
      );
 
 CREATE TABLE `book` (
-    `book_id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` varchar(255) NOT NULL,
     `writer` varchar(255) NOT NULL,
-    `date_published` int(10) NOT NULL,
+    `date_published` datetime NOT NULL,
     `quantity` int(10) NOT NULL
     );
 
 CREATE TABLE `register` (
     `book_id` int(10) unsigned NOT NULL,
-    `student_id` int(10) unsigned NOT NULL
+    `student_id` int(10) unsigned NOT NULL,
     `name` varchar(255) NOT NULL,
     `writer` varchar(255) NOT NULL,
     `date_published` int(10) NOT NULL,
     `quantity` int(10) NOT NULL
     );
+    
+    INSERT INTO book (name, writer, date_published, quantity)
+VALUES("creates ", "javascript", "2022-8-22 17:19:16", 3 
+);
